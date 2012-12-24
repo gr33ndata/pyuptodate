79fea2fabff44a0345632538d92746be57e3db4b
Tarek Amr <gr33ndata@yahoo.com>
2012-12-24 23:36:00 +0000

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
 setup(
-	name='PyUptodate',
+	name='pyuptodate',
 	version='0.1.0',
 	author='Tarek Amr',
 	author_email='',
+    url='https://github.com/gr33ndata/pyuptodate',
 	packages=['pyuptodate'],
+    scripts=['pyuptodate/pyuptodate.py'],
 	license='LICENSE.txt',
 	description='Check all installed Python modules and update them',
 	long_description=open('README.rst').read(),
